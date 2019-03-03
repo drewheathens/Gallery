04125027f62defbec06130b8115df9b31c57a12b
drewheathens <mudavadie@gmail.com>
2019-03-03 19:01:02 +0300

    modified urls

--- photos/urls.py
+++ photos/urls.py
@@ -11,7 +11,7 @@ urlpatterns=[
     #  return news from those dates
     url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_photos,name = 'pastPhotos'),
     url(r'^search/', views.search_results, name='search_results'),
-    url(r'^images/(\d+)',views.images,name ='images')
+    
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
