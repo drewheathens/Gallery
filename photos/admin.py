8f68b80ff68c2e5155f03eea13340bee76cf6ea1
drewheathens <mudavadie@gmail.com>
2019-03-03 17:54:40 +0300

    modified admin

--- photos/admin.py
+++ photos/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Uploader,Images,tags
 
 # Register your models here.
+class ArticleAdmin(admin.ModelAdmin):
+    filter_horizontal =('tags',)
+admin.site.register(Uploader)
+admin.site.register(Images)
+admin.site.register(tags)
