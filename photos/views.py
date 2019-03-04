4dbff15517b0420e5e42d928b9c4648e3030bcba
drewheathens <mudavadie@gmail.com>
2019-03-04 17:27:35 +0300

    modifed views, pics showing

--- photos/views.py
+++ photos/views.py
@@ -7,22 +7,19 @@ from decouple import config,Csv
 # Create your views here.
 def images(request):
     images = Image.objects.all()
-    # ALLOWED_HOSTS = config('ALLOWED_HOSTS')
-  
-
-   
+       
     return render(request,"images.html", {"images":images})
 
 def search_results(request):
 
-    if 'images' in request.GET and request.GET["image"]:
-        search_term = request.GET.get("image")
-        searched_images = Images.search_by_title(search_term)
+    if 'images' in request.GET and request.GET["images"]:
+        search_term = request.GET.get("images")
+        searched_images = Image.search_by_category(search_term)
         message = f"{search_term}"
 
         return render(request, 'search.html',{"message":message,"images": searched_images})
 
     else:
-        message = "You haven't searched for any image"
+        message = "Doesn't match any image"
         return render(request, 'search.html',{"message":message})
 
